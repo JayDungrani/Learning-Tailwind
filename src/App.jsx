aacdb36b83fbabc120a72d1e79169a8e6608ec93
JayDungrani <allinoneshoppingaffiliate@gmail.com>
2024-10-04 10:41:50 +0530

    Spacing

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Typography from './Components/Typography'
+import Spacing from './Components/Spacing'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <Typography/>
+      {/* <Typography/> */}
+      <Spacing/>
     </>
   )
 }
---
+++ src/Components/Spacing.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Spacing = () => {
+  return (
+    <div>
+      <div className='bg-green-400 p-5'>Content</div>
+      <div className='bg-red-400 px-5'>Content</div>
+      <div className='bg-blue-400 py-5'>Content</div>
+      <div className='bg-orange-400 pt-5'>Content</div>
+      <div className='bg-pink-400 m-5'>Content</div>
+      <div className='bg-purple-400 my-5 mx-10 p-5'>Content</div>
+    </div>
+  )
+}
+
+export default Spacing
